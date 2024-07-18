a095f4ef37fe5b2d2fd77cfea44307541b5887dc
Austin <lyvuaustin@gmail.com>
2024-07-18 16:44:03 -0500

    Added Tracing

--- app.py
+++ app.py
@@ -5,8 +5,12 @@ with sync_playwright() as playwright:
     # Launch Browser
     browser = playwright.chromium.launch(headless=False, slow_mo=700)
 
+    # Playwright Tracing
+    context = browser.new_context()
+    context.tracing.start(screenshots=True, snapshots=True, sources=True)
+
     # Create a new page/tab
-    page = browser.new_page()
+    page = context.new_page()
 
     # Visit playwright website
     page.goto("https://playwright.dev/python/")
@@ -17,6 +21,9 @@ with sync_playwright() as playwright:
 
     # Get URL
     print("Docs: ", page.url)
+
+    # Closes Tracing
+    context.tracing.stop(path = "trace.zip")
 
     # Close browser
     browser.close()
